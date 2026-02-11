8080e4a89b31ab22d445786412afefbbe5eb869b
MarguaInfa <marguainfant@outlook.com>
2026-02-11 10:01:14 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -283,7 +283,7 @@ export default function App() {
       </div>
 
       {/* LISTA DE PRODUCTOS */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
         {filtrados.slice(0, limite).map((p, index) => {
           const tabla = generarTablaPrecios(p);
 
